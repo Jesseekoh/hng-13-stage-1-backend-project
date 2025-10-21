7ef52251a77a37e3fc584306790f28ee9707c3d4
Jesse Ekoh-Ordan <jesseekoh@gmail.com>
2025-10-21 16:17:25 +0100

    Update README with String Analysis API docs

--- src/app.ts
+++ src/app.ts
@@ -3,13 +3,7 @@ import type { Request, Response, NextFunction } from "express";
 import { ZodError } from "zod";
 import rateLimit from "express-rate-limit";
 import cors from "cors";
-import {
-  analyzeStringController,
-  getFilteredStringsController,
-  getStringController,
-  deleteStringController,
-  filterByNaturalLanguage,
-} from "./controllers/stringController";
+import stringRoutes from "./routes/string.routes";
 import { pinoHttp } from "pino-http";
 import logger from "./utils/logger";
 const app = express();
@@ -19,21 +13,17 @@ const limiter = rateLimit({
   max: 100, // limit each IP to 100 requests per windowMs
 });
 
-app.use(pinoHttp({ logger }));
+// app.use(pinoHttp({ logger }));
 
-app.use(cors());
-app.use(express.json());
 app.use(limiter);
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.post("/strings", analyzeStringController);
-app.get("/strings", getFilteredStringsController);
-app.get("/strings/filter-by-natural-language", filterByNaturalLanguage);
-app.get("/strings/:value", getStringController);
-app.delete("/strings/:value", deleteStringController);
+app.use("/strings", stringRoutes);
 
 app.use((err: unknown, req: Request, res: Response, next: NextFunction) => {
   logger.error(err, "Error occurred");
